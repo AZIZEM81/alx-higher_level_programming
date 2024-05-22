2568e5d17b04b6577020f3ae047eeb88f6d0c14e
AZIZEM81 <eaziz428@gmail.com>
2024-05-22 09:16:40 -0700

    azizsqladva

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,6 @@
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-GROUP BY city
+-- 101-avg_temperatures.sql
+SELECT c.city, AVG(t.value) AS avg_temp
+FROM temperatures t
+JOIN cities c ON t.city_id = c.city_id
+GROUP BY c.city
 ORDER BY avg_temp DESC;
