988002dc551bba369b97bdf93bb013296171408c
AZIZEM81 <eaziz428@gmail.com>
2024-06-18 15:22:25 -0700

    azizjvs

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,19 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(number + 1);
+const myObject = {
+  type: 'object',
+  value: 12
 };
+
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
