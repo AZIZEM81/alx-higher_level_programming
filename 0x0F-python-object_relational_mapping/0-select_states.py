6017151f6e3754a284b93320a583042184c7610a
AZIZEM81 <eaziz428@gmail.com>
2024-06-23 15:50:15 -0700

    aziz

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa
+This module lists all states from the database hbtn_0e_0_usa.
+It connects to a MySQL server running on localhost at port 3306.
 """
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Check if all 3 arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Get MySQL connection parameters from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+def list_states(username, password, db_name):
+    """
+    Connects to the MySQL database and lists all states.
 
+    Args:
+    username (str): MySQL username
+    password (str): MySQL password
+    db_name (str): Database name
+
+    Returns:
+    None
+    """
     try:
         # Establish a connection to the MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+        )
 
         # Create a cursor object to execute SQL queries
         cursor = db.cursor()
@@ -44,3 +53,18 @@ if __name__ == "__main__":
             cursor.close()
         if db:
             db.close()
+
+
+if __name__ == "__main__":
+    # Check if all 3 arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get MySQL connection parameters from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    list_states(username, password, db_name)
