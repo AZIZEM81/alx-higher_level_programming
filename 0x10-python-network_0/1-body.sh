c22e7cea24baa822c6f9557c78df8b45d20a3a4b
AZIZEM81 <eaziz428@gmail.com>
2024-07-05 12:29:59 -0700

    curl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# GET request to URL, display body of 200 status response
+# GET request and display body of 200 status response
 curl -s -o /dev/null -w '%{http_code}' "$1" | grep -q "200" && curl -s "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display allowed HTTP methods
-curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
+curl -s -I -X OPTIONS "$1" | grep "Allow" | cut -d " " -f 2-
